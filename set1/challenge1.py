a3add7317f49ded99ccf55b7182505cb0c04e428
jhvanderbeek <jessehvanderbeek@gmail.com>
2020-03-31 13:51:01 +1100

    Used functions from helper.py

--- set1/challenge1.py
+++ set1/challenge1.py
@@ -1,14 +1,10 @@
 import sys
+import helper
 
 hexstr = sys.argv[1]
 
-#a = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+bytearr = helper.hexstrtobytes(hexstr)
 
-# First split hexstr up into two character chunks (so each chunk is a byte)
-from math import ceil
-hexarr = [ int(hexstr[2*i: 2*(i+1)],16) for i in range(ceil(len(hexstr)/2)) ]
-# Convert the hex numbers into bytes
-bytearr = bytes(hexarr)
 # Convert the bytes to base64 numbers
 from base64 import b64encode
 sfbytes = b64encode(bytearr)
