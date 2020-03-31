c16f99e88ed404efb42898e52516929186fee684
jhvanderbeek <jessehvanderbeek@gmail.com>
2020-03-31 15:27:45 +1100

    added bytestohexstr and xor functions

--- set1/helper.py
+++ set1/helper.py
@@ -1,4 +1,6 @@
 from math import ceil
+from functool import reduce
+
 def hexstrtobytes(hexstr):
     """Converts a string of hex characters into an array of bytes"""
     # First split hexstr up into two character chunks (so each chunk is a byte)
@@ -6,3 +8,16 @@ def hexstrtobytes(hexstr):
     # Convert the hex numbers into bytes
     bytearr = bytes(hexarr)
     return bytearr
+
+def bytestohexstr(bytearr):
+    """Converts an array of bytes to a string of hex characters"""
+    hexarr = [ "{:02x}".format(i) for i in bytearr ]
+    hexstr = reduce( str.__add__, hexarr )
+    return hexstr
+
+def xor(bytes1, bytes2):
+    """Does an elementwise xor on a bytes object and returns the
+    result in a bytes object"""
+    n = min( len(bytes1), len(bytes2) )
+    result = [ x^y for (x,y) in zip(bytes1, bytes2) ]
+    return bytes(result)
