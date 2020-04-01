83374cf6b0d9244420a9c52f834f4354212836cb
jhvanderbeek <jessehvanderbeek@gmail.com>
2020-04-02 10:39:26 +1100

    added functionality to rotate bits

--- set1/aes.c
+++ set1/aes.c
@@ -1,4 +1,4 @@
-/*
+/**
  * Some code for aes encryption, sticking to 128 bit keys for now
  * 
  * https://www.samiam.org/key-schedule.html
@@ -7,40 +7,38 @@
 typedef unsigned char Byte;
 
 /**
- * Rotates an array of Bytes of length n to the left.
+ * Rotates an array of Bytes of length n to the left by t bits.
  * 
  * Takes an array of bytes 'in' of length n and moves the ith element to the
- * (i-1)th position. The 0th element is moved to the end of the array.
+ * (i-t)th position. Bits to the left of the tth position roll around to the 
+ * right.
  * @param in    The array of Bytes
  * @param n     The size of the array
+ * @param t     The number of bits to shift
  * 
  * @return out  The rotated array
  */
-Byte *rotateBytesLeft( Byte *in, size_t n ) {
+Byte *rotateBytesLeft( Byte *in, size_t n, size_t t) {
     Byte *out = malloc( sizeof(Byte) * n );
-    out[n] = in[0];
-    for (size_t i = 1; i < n; ++i) {
-        out[i-1] = in[i];
-    }
+    out = (*in << t) || (*in >> (n-t));
     return out;
 }
 
 /**
- * Rotates an array of Bytes of length n to the right.
+ * Rotates an array of Bytes of length n to the right by t bits.
  * 
  * Takes an array of bytes 'in' of length n and moves the ith element to the
- * (i+1)th position. The nth element is moved to the start of the array.
+ * (i+t)th position. Bits to the right of the (n-t)th position roll around to
+ * the left.
  * @param in    The array of Bytes
  * @param n     The size of the array
+ * @param t     The number of bits to shift
  * 
  * @return out  The rotated array
  */
-Byte *rotateBytesRight( Byte *in, size_t n ) {
+Byte *rotateBytesRight( Byte *in, size_t n, size_t t ) {
     Byte *out = malloc( sizeof(Byte) * n );
-    out[0] = in[n];
-    for (size_t i = 1; i < n; ++i) {
-        out[i] = in[i-1];
-    }
+    out = (*in >> t) || (*in << (n-t));
     return out;
 }
 
