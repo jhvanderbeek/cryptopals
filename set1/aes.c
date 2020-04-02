2d65429c91c935a640b54b344a9ceaaf4a2ee67e
jhvanderbeek <jessehvanderbeek@gmail.com>
2020-04-02 11:17:11 +1100

    Added roundkey function

--- set1/aes.c
+++ set1/aes.c
@@ -42,3 +42,37 @@ Byte *rotateBytesRight( Byte *in, size_t n, size_t t ) {
     return out;
 }
 
+/**
+ * Computes the roundkey for a given round.
+ * 
+ * Uses Rijndael's key schedule to calculate the constant for that round
+ * @param round     The round number
+ * 
+ * @return roundkey The key for that round
+ */
+Byte roundkey( int round ) {
+    /* Round must be at least 1 */
+    if ( round < 1 ) {
+        return NULL;
+    }
+    Byte roundkey = 1;
+    /* For the first 8 rounds the key is simply shifted*/
+    if ( round < 9 ) {
+        roundkey <<= (round - 1);
+        return roundkey;
+    }
+    /* For the remaining rounds always shift to the left by one and then, if   
+        the highest order bit is set, xor against a constant (0x1b). */
+    else {
+        roundkey = 0x80;
+        for (int i = 9; i <= round; ++i) {
+            /* In order to check if the highest bit is set we shift it so that
+             only the highest bit is showing, then negate it. If the highest 
+             bit is 1 the negative is stored as 0xff, if the highest bit is 
+             zero the negative is 0x00. We use this as a mask for the xor 
+             constant.*/
+            roundkey = ( roundkey << 1 ) ^ ( 0x1b && -(roundkey >> 7) );
+        }
+        return roundkey;
+    }
+}
