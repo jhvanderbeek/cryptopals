c1c6017ff38008452cd601ae600c1699d3b16364
jhvanderbeek <jessehvanderbeek@gmail.com>
2020-04-02 12:48:47 +1100

    Added rotateWordLeft/Right and testing

--- set1/aes.c
+++ set1/aes.c
@@ -4,8 +4,11 @@
  * https://www.samiam.org/key-schedule.html
  */
 #include <stdlib.h>
+#include <stdio.h>
 #include <assert.h>
 #define BITS_PER_BYTE   8
+#define BYTES_PER_WORD  4  
+// #define TEST
 typedef unsigned char Byte;
 
 /**
@@ -41,10 +44,45 @@ Byte rotateByteRight( Byte in, int n ) {
     out = (in >> n) | (in << (BITS_PER_BYTE - n));
     return out;
 }
+
+/**
+ * Rotates a word of bytes to the left by one byte.
+ * 
+ * Moves bytes of a word to the left by one. The 0th byte is moved to the end 
+ * of the word. Does so in place.
+ * @param in    The word to be rotated
+ */
+void rotateWordLeft(Byte *in) {
+        Byte tmp;
+        tmp = in[0];
+        for(size_t i = 0; i < BYTES_PER_WORD - 1; ++i) 
+                in[i] = in[i + 1];
+        in[BYTES_PER_WORD - 1] = tmp;
+        return;
+}
+
+/**
+ * Rotates a word of bytes to the right by one byte.
+ * 
+ * Moves bytes of a word to the right by one. The last byte is moved to the
+ * beginning of the word. Does so in place.
+ * @param in    The word to be rotated
+ */
+void rotateWordRight(Byte *in) {
+        Byte tmp;
+        tmp = in[BYTES_PER_WORD - 1];
+        for(size_t i = BYTES_PER_WORD - 1; i > 0; --i) 
+                in[i] = in[i - 1];
+        in[0] = tmp;
+        return;
+}
+
 /**
  * Computes the roundkey for a given round.
  * 
- * Uses Rijndael's key schedule to calculate the constant for that round
+ * Uses Rijndael's key schedule to calculate the constant for that round. 
+ * This is equivalent to multiplying by x in the field 
+ * \mathbb{F}_2[x] / (x^8 + x^4 + x^3 + x + 1)
  * @param round     The round number
  * 
  * @return roundkey The key for that round
@@ -76,6 +114,7 @@ Byte roundkey( int round ) {
     }
 }
 
+
 # if defined TEST
 int main () {
     /* Testing for rotateByteLeft and rotateByteRight*/
@@ -101,8 +140,32 @@ int main () {
         assert( rotateByteLeft(testByte, 2 + BITS_PER_BYTE) == rotateByteLeft(testByte, 2) );
         assert( rotateByteRight(testByte, 2 + BITS_PER_BYTE) == rotateByteRight(testByte, 2) );
     }
+    printf("rotateByteLeft and rotateByteRight passed testing!\n");
+
+    /* Testing for rotateWordLeft and rotateWordRight*/
+    {
+        Byte word[] = {0x01, 0x02, 0x03, 0x04};
+        Byte testword1[] = {0x02, 0x03, 0x04, 0x01};
+        Byte testword2[] = {0x04, 0x01, 0x02, 0x03};
+        rotateWordLeft(word);
+        for (size_t i = 0; i < 4; ++i) {
+            assert(word[i] == testword1[i]);
+        }
+        rotateWordRight(word);
+        rotateWordRight(word);
+        for (size_t i = 0; i < 4; ++i) {
+            assert(word[i] == testword2[i]);
+        }
+    }
+    printf("rotateWordLeft and rotateWordRight passed testing!\n");
 
     /* Testing for roundkey */
-    
+    {
+        Byte testvectors[] = {0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a};
+        for (size_t i; i < 16; ++i) {
+            assert( roundkey(i) == testvectors[i] );
+        }
+    }
+    printf("roundkey passed testing!\n");
 }
 # endif
